6f1b1d6f2a97be74935e65c8602dcf8aebd2d2fe
andre-lopss <andrelneres@hotmail.com>
2022-10-02 21:00:49 -0300

    Projeto para interagir a linguagem com o HTTP utilizando OO e Json no manejo dos dados

---
+++ InteracaoHTTP/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InteracaoHTTP
+{
+    class Program
+    {
+        enum Menu { Requisicaotds = 1, RequisicaoUnica, Sair}
+        
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+
+            while (escolheuSair == false)
+            {
+                Console.WriteLine("1 - Requisitar todas tarefas\n2 - Requisitar uma única tarefa\n3 - Sair");
+                string coletaOpc = Console.ReadLine();
+                int transformaOpc = int.Parse(coletaOpc);
+                Menu escolha = (Menu)transformaOpc;
+                if (transformaOpc > 0 && transformaOpc < 3)
+                {
+                    switch (escolha)
+                    {
+                        case Menu.Requisicaotds:
+                            ReqLista();
+                            break;
+                        case Menu.RequisicaoUnica:
+                            ReqUnica();
+                            break;
+                        case Menu.Sair:
+                            escolheuSair = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    escolheuSair = true;
+                }
+                Console.Clear();
+            }
+        }
+
+        static void ReqLista()
+        {
+            Console.Clear();
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
+            requisicao.Method = "GET";
+
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                foreach (Tarefa tarefa in tarefas)
+                {
+                    tarefa.Exibir();
+                }
+              
+                stream.Close();
+                resposta.Close();
+
+                Console.ReadLine();
+            }
+        }
+
+        static void ReqUnica()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o ID da tarefa desejada: ");
+            string id = Console.ReadLine();   
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/"+id);
+            requisicao.Method = "GET";
+
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                Tarefa tarefa = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+
+                tarefa.Exibir();
+
+                stream.Close();
+                resposta.Close();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ InteracaoHTTP/Tarefa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteracaoHTTP
+{
+    class Tarefa
+    {
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Exibir()
+        {
+            Console.WriteLine("OBJETO TAREFA");
+            Console.WriteLine($"User Id: {userId}");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Finalizou?: {completed}");
+            Console.WriteLine("==================================================");
+            Console.WriteLine("");
+
+        }
+    }
+}
